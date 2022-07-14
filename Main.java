e4788a2604044ca60588d9c0981f7f731a460574
KotaShin <s1290075@u-aizu.ac.jp>
2022-07-14 17:52:14 +0900

    User name added

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
     
-class Main {
+class Main2 {
     public static void main(String[] args) {
+	String name;
 	Random r = new Random();
 	Coin c = new Coin();
+	Scanner sc = new Scanner(System.in);
 
-	System.out.println("Tossing...");
+	System.out.println("Who are you?");
+	System.out.print(">");
+	name = sc.nextLine();
+	System.out.println("Hello,"+name+"!");	
+	
+	System.out.println("Tossing a coin...");
 	for(int i = 1; i <= 3; i++) {
 	    int x = r.nextInt(2);
 	    System.out.print("Round "+i+": ");
